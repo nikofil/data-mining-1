3b5f1e4713831613a4626937562a60bcc4fcc33b
Nikos Filippakis <aesmade@gmail.com>
2016-05-12 18:43:41 +0200

    _imagingft error

--- word_cloud/word_cloud.py
+++ word_cloud/word_cloud.py
@@ -1,4 +1,18 @@
-import wordcloud;
+from wordcloud import WordCloud
+import pandas
 
 if __name__ == '__main__':
-    print "hello world"
+    df = pandas.read_csv('../datasets/train_set.csv', sep='\t')
+
+    categories = dict()
+
+    for index, row in df.iterrows():
+        cat = row['Category']
+        if cat not in categories:
+            categories[cat] = ''
+        categories[cat] += row['Content'] + ' '
+
+    for (cat, texts) in categories.iteritems():
+        print cat, texts
+        wc = WordCloud().generate(texts)
+        wc.to_file('generated_wordclouds/' + cat)
