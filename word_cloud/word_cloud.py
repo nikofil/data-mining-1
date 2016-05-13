5c14efdd5aeb09dce2bc475d5c13c54e0003cfe3
Nikos Filippakis <aesmade@gmail.com>
2016-05-13 10:59:30 +0200

    Requirements and working wordcloud

--- word_cloud/word_cloud.py
+++ word_cloud/word_cloud.py
@@ -1,11 +1,46 @@
-from wordcloud import WordCloud
-import pandas
+#!/usr/bin/python
+from wordcloud import WordCloud, STOPWORDS
+import os, pandas, argparse
 
 if __name__ == '__main__':
-    df = pandas.read_csv('../datasets/train_set.csv', sep='\t')
+
+    # arguments for input/output files
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', help='Input CSV file',
+                        default='../datasets/train_set.csv')
+    parser.add_argument('-o', '--output', help='Output directory',
+                        default='generated_wordclouds/')
+    parser.add_argument('-d', '--display', help='Display created wordclouds',
+                        action='store_true', default=False)
+    args = parser.parse_args()
+
+    try:
+        df = pandas.read_csv(args.input, sep='\t')
+
+        dir_out = args.output
+        if dir_out[-1] != '/':
+            dir_out += '/'
+
+        # make the output dir if it doesn't exist
+        if not os.path.exists(dir_out):
+            os.mkdir(dir_out, 0755)
+
+        print "Input file:", args.input
+        print "Output directory:", dir_out
+        
+    except IOError as e:
+        print 'Error:', str(e)
+        exit(1)
 
     categories = dict()
 
+    # words to ignore
+    stopwords = STOPWORDS.copy()
+    stopwords.add('said')
+    stopwords.add('will')
+    stopwords.add('new')
+
+    # gather all the content in one string for each category
     for index, row in df.iterrows():
         cat = row['Category']
         if cat not in categories:
@@ -13,6 +48,9 @@ if __name__ == '__main__':
         categories[cat] += row['Content'] + ' '
 
     for (cat, texts) in categories.iteritems():
-        print cat, texts
-        wc = WordCloud().generate(texts)
-        wc.to_file('generated_wordclouds/' + cat)
+        path_out = dir_out + cat + '.jpg'
+        print 'Generating wordcloud for category', cat, 'at', path_out
+        wc = WordCloud(stopwords=stopwords).generate(texts)
+        if args.display:
+            wc.to_image().show()
+        wc.to_file(path_out)
